d1f6894edf2f689611883af2304c92b71708a096
Pradeep Pandey <90238672+UtechP-40@users.noreply.github.com>
2024-09-25 01:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function volume_sphere() {
     //Write your code here
+    let r = document.getElementById("radius").value;
+    document.getElementById("volume").value = 4/3*Math.PI*r**3;
   
 } 
-
+//Calculate the volume of the sphere using the formula: [ V = \frac{4}{3} \pi r^3 ]
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
